1c595a09a562c16eba14a33989b0303e7be8bdde
hw18a001 <>
2020-11-17 00:40:01 +0900

    新規登録、ログイン、マイページなど追加。ユーザーのDBも作成しました。

--- boitter/config/routes.rb
+++ boitter/config/routes.rb
@@ -3,10 +3,17 @@ Rails.application.routes.draw do
   #トップページ
   get '/' => 'top#top'
   
+  
   #tweet
   #resources :tweets    #自動的割り振りできたが、データベースが反映されなかった
   get 'tweets/index' => 'tweets#index'  #一覧
   get 'tweets/new' => 'tweets#new'      #新規
   get 'tweets/:id' => 'tweets#show'     #詳細
   post 'tweets/create' => 'tweets#create'
+  
+  #user
+  get 'user/signup' => 'user#signup'  #新規登録
+  get 'user/login' => 'user#login'    #ログイン
+  get 'user/mypage/:id' => 'user#mypage'  #マイページ
+  
 end
