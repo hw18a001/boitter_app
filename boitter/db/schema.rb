7e3618d3d81e7e36ad5d7d1669c4efa87800e9d3
hw18a001 <>
2020-11-28 17:23:49 +0900

    userテーブルにintro(自己紹介)カラムを追加しました。

--- boitter/db/schema.rb
+++ boitter/db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_11_12_072546) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "intro"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
